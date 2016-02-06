a9feeca7afa4bcc377a962b7eaa682f06dd1b8ce
sparklyballs <sparklyballs@gmail.com>
2016-02-06 13:21:34 +0000

    try this on for size, put legacy test in services file also to set app path

--- init/50_check_for_config.sh
+++ init/50_check_for_config.sh
@@ -2,13 +2,13 @@
 
 mkdir -p /config/.sync
 
-[[ ! -f /config/btsync.conf ]] && cp /defaults/btsync.conf.stable /config/stable-btsync.conf
+[[ ! -f /config/latest-btsync.conf ]] && cp /defaults/btsync.conf.stable /config/latest-btsync.conf
 
 if [[ "$LEGACY" == "true" ]]; then
 [[ ! -f /config/legacy-btsync.conf ]] && cp /defaults/btsync.conf.legacy /config/legacy-btsync.conf
-export APP_PATH="/app/btsync-1.4" CONF_PATH="/config/legacy-btsync.conf"
+echo "using legacy version, config file is \"/config/legacy-btsync.conf\"" 
 else
-export APP_PATH="/app/btsync-latest" CONF_PATH="/config/stable-btsync.conf"
+echo "using stable version, config file is \"/config/latest-btsync.conf\"" 
 fi
 
 chown abc:abc -v /config/*btsync.conf
