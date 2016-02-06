773c3ce2d9d20579f9c1d068cd7a8ee7b93841c6
sparklyballs <sparklyballs@gmail.com>
2016-02-06 12:55:14 +0000

    possible solution to eliminate linking

--- init/50_check_for_config.sh
+++ init/50_check_for_config.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-
-
-if [ ! -f /config/btsync.conf ]; then
-	echo -n "btsync.conf not found Will install: "
-	if [ "$LEGACY" == "true" ]; then
-		echo  "version 1.4"
-		ln -s /app/btsync /app/btsync-1.4
-		cp -v /defaults/btsync.conf.legacy /config/btsync.conf
-	else
-		echo "latest version"
-		ln -s /app/btsync /app/btsync-latest
-		cp -v /defaults/btsync.conf.standard /config/btsync.conf
-	fi
-	chown abc:abc -v /config/btsync.conf
-fi
-
-if  [ ! -f /config/btsync.conf.sample ]; then
-	echo "Adding sample config to \"/config/btsync.conf.sample\" so you can find options there."
-	/sbin/setuser abc /app/btsync --dump-sample-config > /config/btsync.conf.sample
-fi
-
 mkdir -p /config/.sync
+
+[[ ! -f /config/btsync.conf ]] && cp /defaults/btsync.conf.stable /config/stable-btsync.conf
+
+if [[ "$LEGACY" == "true" ]]; then
+[[ ! -f /config/legacy-btsync.conf ]] && cp /defaults/btsync.conf.legacy /config/legacy-btsync.conf
+export APP_PATH="/app/btsync-1.4" CONF_PATH="/config/legacy-btsync.conf"
+else
+export APP_PATH="/app/btsync-latest" CONF_PATH="/config/stable-btsync.conf"
+fi
+
+chown abc:abc -v /config/*btsync.conf
+
+
+
+
